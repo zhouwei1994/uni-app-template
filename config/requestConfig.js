8e7db233e8f4ee95365abddbc8262b52084db3be
momo3207778@126.com <momo3207778@126.com>
2021-02-22 16:07:09 +0800

    提交

--- config/requestConfig.js
+++ config/requestConfig.js
@@ -28,6 +28,8 @@ let $http = new request({
 	baseUrl: base.baseUrl,
 	//服务器本地上传文件地址
 	fileUrl: base.baseUrl,
+	// 服务器上传图片默认url
+	defaultUploadUrl: "api/common/v1/upload_image",
 	//设置请求头（如果使用报错跨域问题，可能是content-type请求类型和后台那边设置的不一致）
 	header: {
 		'Content-Type': 'application/json;charset=UTF-8',
